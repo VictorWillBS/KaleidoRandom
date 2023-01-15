4fa397b995a5e34acb45bb52ad6e48337b74671d
victor <coffiekingb@gmail.com>
2023-01-14 22:59:28 -0300

    :sparkles: Feat: Create Randomize Button

---
+++ src/components/Button/randomizeButton.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import { H3 } from "../Episodes/episode;"
+
+export function RandomizeButton({setUpdate,update}){
+  const refreshList = ()=>{
+    setUpdate(!update);
+  }
+  return(
+    <Button
+    onClick={refreshList}> 
+      <ButtonText>
+        Nova Ordem Aleatória
+      </ButtonText>
+    </Button>
+  )
+}
+
+const Button = styled.button`
+  margin: 40px 0;
+  max-width:320px;
+  width:100%;
+  height:75px;
+  border: none;
+  border-radius:15px;
+  background-color:#0B7A2A;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  cursor:pointer;
+  :active{
+    filter:brightness(1.1)
+  }
+`
+const ButtonText = styled(H3)`
+  font-size:20px;
+  margin-top:0;
+`
