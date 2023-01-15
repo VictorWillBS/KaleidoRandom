b96dbf6670665ff38f4d1cd721746fd32923c003
victor <coffiekingb@gmail.com>
2023-01-14 22:59:59 -0300

    :sparkles: Feat: Create Title Component

---
+++ src/components/Title/title.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import { Container } from "../Content/content";
+
+export function Title({children}){
+  return(
+    <Container config={"height: initial;"}>
+      <StyledH1>
+        {children}
+      </StyledH1>
+    </Container>
+  )
+}
+
+const StyledH1=  styled.h1`
+ font-family: 'Inter', sans-serif;
+ font-size:50px;
+ color:white;
+`
